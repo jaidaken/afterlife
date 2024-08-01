3636d5c662070477bdca1729a493ce765a54c627
Jaidaken <jamiehewitt@pm.me>
2024-08-01 17:42:45 +0000

    scrollbar resize

--- src/components/CustomScrollbar.tsx
+++ src/components/CustomScrollbar.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
 interface CustomScrollbarProps {
-  children: React.ReactNode;
-  style?: React.CSSProperties;
+	children: React.ReactNode;
+	style?: React.CSSProperties;
 }
 
 const CustomScrollbar: React.FC<CustomScrollbarProps> = ({ children }) => {
-  return (
-		<Scrollbars className='scroll-div flex-grow' renderThumbVertical={props => <div {...props} className="thumb-vertical" />}
-		renderTrackVertical={props => <div {...props} className="track-vertical" />}
-		renderView={props => <div {...props} className="scroll-content" />}>
-      {children}
-    </Scrollbars>
-  );
+	const [hideTrack, setHideTrack] = useState(false);
+
+	useEffect(() => {
+		const thumbVertical = document.querySelector('.thumb-vertical');
+		setHideTrack(false);
+
+		if (thumbVertical) {
+			const observer = new ResizeObserver(() => {
+				const height = thumbVertical.clientHeight;
+				console.log('Height of thumb-vertical:', height);
+				if (height === 0) {
+					setHideTrack(true);
+				} else {
+					setHideTrack(false);
+				}
+			});
+
+			observer.observe(thumbVertical);
+
+			return () => {
+				observer.disconnect();
+			};
+		}
+	}, []);
+
+	return (
+		<Scrollbars
+			className='scroll-div flex-grow'
+			renderThumbVertical={props => <div {...props} className="thumb-vertical" />}
+			renderTrackVertical={props => <div {...props} className={`track-vertical ${hideTrack ? 'hide-track' : ''}`} />}
+			renderView={props => <div {...props} className="scroll-content" />}
+		>
+			{children}
+		</Scrollbars>
+	);
 };
 
 export default CustomScrollbar;
