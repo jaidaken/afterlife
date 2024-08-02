a69b54ac9f37c102b1b075ba15a9f5f3a8ddc6f9
Jaidaken <jamiehewitt@pm.me>
2024-08-02 20:09:11 +0000

    max width on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => (
 					<div className='px-32'>
 
 						<div className="flex justify-center items-center bg-gray-700 w-screen">
-							<div className="flex justify-center items-center my-4 gap-4 w-3/4">
+							<div className="flex justify-center items-center my-4 gap-4 w-3/4 max-w-7xl">
 								<img src={badge} alt="Meteor Shower" className="col-span-1" width="300" height='auto' />
 								<div className="p-4 w-3/4 ">
 									<h1 className="font-bold mb-4 text-4xl">Welcome to Blair County!</h1>
@@ -26,7 +26,7 @@ const Home: React.FC = () => (
 						</div>
 
 						<div className="flex justify-center items-center bg-gray-800 w-screen">
-							<div className="flex justify-center items-center my-4 gap-4 w-3/4">
+							<div className="flex justify-center items-center my-4 gap-4 w-3/4 max-w-7xl">
 								<div className="p-4 w-3/4 ">
 									<p className="mb-6">Blair County is proud to host a once-in-a-lifetime event that has captured the attention of stargazers and thrill-seekers from across the globe. The Blair County Meteor Shower promises to be an extraordinary celestial event unlike any other.</p>
 									<p className="mb-6">For months, we've been planning and preparing to bring the most spectacular meteor shower ever witnessed to the good people of Blair County, and it's esteemed visitors. This event is designed to be the highlight of your decade, with planned activities and exclusive viewing spots throughout the county.</p>
@@ -50,7 +50,7 @@ const Home: React.FC = () => (
 						</div>
 
 						<div className="flex justify-center items-center bg-gray-900 w-screen">
-							<div className="flex flex-col justify-center items-center my-4 gap-4 w-3/4">
+							<div className="flex flex-col justify-center items-center my-4 gap-4 w-3/4 max-w-7xl">
 								<img src={afterlife} alt="Afterlife" className="mx-auto mb-4" width="full" />
 								<p className="mb-4 text-center w-3/4">
 									Afterlife is a Project Zomboid server focused on delivering a quality roleplay server that revels in the themes of the supernatural. With a fully customised map and no more stereotypical zombies, Afterlife prides itself on its uniqueness and is excited to welcome you in!
